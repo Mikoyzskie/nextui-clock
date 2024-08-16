6465f7eaf5bcae97821c98abbea974c3e5c301a2
Mikoyzskie <rapidstrike13@gmail.com>
2024-08-16 15:17:17 +0800

    chore: + websocket password verify

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,25 +1,70 @@
 "use client";
-import { useMemo } from "react";
+import { FormEvent, useMemo, useState, useRef } from "react";
 import { io } from "socket.io-client";
+
+import { IEmployees } from "@/app/types";
+
 export default function Page() {
-  // const [socket, setSocket] = useState<any>(undefined)
+  const [employees, setEmployees] = useState<IEmployees[] | undefined>();
+  const [username, setUsername] = useState("");
+  const [userAvailable, setUserAvailable] = useState<IEmployees | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formRef = useRef<HTMLFormElement>(null);
+
   const socket = useMemo(() => io("http://localhost:4000"), []);
 
   socket.on("connect", () => {
     // console.log("connected");
   });
 
-  // socket.on("message", (data: string) => {
-  //   console.log(data);
-  // });
+  socket.on("EMPLOYEE_LIST", (data: string) => {
+    const employees = JSON.parse(data);
 
-  const handleServerTest = () => {
-    socket.emit("test", "hello server, this is client");
+    setEmployees(employees);
+  });
+
+  socket.on("USER_AVAILABLE", (data: string) => {
+    setUserAvailable(JSON.parse(data));
+  });
+
+  socket.on("LOADING_DONE", (data) => {
+    setIsLoading(data);
+  });
+
+  socket.on("USER_LOGGED", () => {
+    formRef.current?.reset();
+  });
+
+  const userExists = employees?.find(
+    (employee) => employee.Employee_Username === username,
+  );
+
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>): void => {
+    setIsLoading(true);
+
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const password = data.get("password");
+
+    if (userExists) {
+      socket.emit("USER_CHECK", userExists.id, password);
+    }
   };
 
   return (
-    <div>
-      <button onClick={handleServerTest}>Test Server</button>
-    </div>
+    <form ref={formRef} onSubmit={handleFormSubmit}>
+      <input
+        required
+        name="username"
+        type="text"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input required name="password" type="password" />
+      {userExists && `${userExists.Clock_Status}`}
+      <button disabled={isLoading} type="submit">
+        {isLoading ? "Loading ..." : "Submit"}
+      </button>
+    </form>
   );
 }
